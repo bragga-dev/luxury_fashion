7b4cad844972f58fff906a313af03a951b53c4b3
bragga-dev <bragawebdevelopment@gmail.com>
2026-09-02 11:37:04 -0300

    feat: Add Order and OrderItem models to payments module

---
+++ luxury_fashion/apps/payments/models/__init__.py
@@ -0,0 +1,15 @@
+from luxury_fashion.apps.payments.models.order_model import Order
+from luxury_fashion.apps.payments.models.order_item_model import OrderItem
+
+
+
+
+
+
+
+
+__all__ = [
+
+    "Order",
+    "OrderItem",
+]
